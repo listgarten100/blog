39ee673245d69e40b35cad94e1df42d0849ba8a9
Vlad Listgarten <listgarten100@gmail.com>
2021-02-09 13:24:47 +0200

    attempt to create columns with the same height

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,6 @@ let burger = document.querySelector('.burger');
 let burgerMenu = document.querySelector('.burger-menu')
 
 burger.addEventListener('click', () => {
-  console.log('hi');
   burger.classList.toggle('active');
   burgerMenu.classList.toggle('active');
 });
